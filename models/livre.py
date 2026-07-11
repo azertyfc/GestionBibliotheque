c9ceec386b2f83e5fab67093aa3012d7a27695b0
p2308181 <diawabdel00@gmail.com>
2026-07-11 12:15:44 +0200

    ajout model livre.py

---
+++ models/livre.py
@@ -0,0 +1,8 @@
+"""Livre model.py
+This module defines the Livre model, which represents a book in the application.
+Attributes:
+    id (int): The unique identifier for the book.
+    title (str): The title of the book.
+    author (str): The author of the book.
+    publication_date (date): The publication date of the book.  
+"""
